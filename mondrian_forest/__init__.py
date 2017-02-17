b6601beb44e45fc192a2920117b198844708cf4c
MechCoder <mks542@nyu.edu>
2017-02-17 15:54:40 -0500

    Add init

--- mondrian_forest/__init__.py
+++ mondrian_forest/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.0.dev0"
+from .tree import DecisionTreeClassifier
+from .tree import DecisionTreeRegressor
+from .tree import ExtraTreeClassifier
+from .tree import ExtraTreeRegressor
+from .tree import export_graphviz
+
+__all__ = ["DecisionTreeClassifier", "DecisionTreeRegressor",
+           "ExtraTreeClassifier", "ExtraTreeRegressor", "export_graphviz"]
