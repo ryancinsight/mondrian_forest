d47a75bf6b990101ec65dafec6a4ce603ea6fae1
MechCoder <mks542@nyu.edu>
2017-03-14 14:13:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ popd
 # Configure the conda environment and put it in the path using the
 # provided versions
 conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
-   numpy scipy scikit-learn
+   numpy scipy scikit-learn cython
 source activate testenv
 
 python --version
